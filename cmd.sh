2052e4e3518936944146a22ece9ac6f30d6c6c61
Serhii Tomko <sergey@tomko.ks.ua>
2022-05-21 15:05:40 +0300

    Добавлен jenkins.yaml

--- cmd.sh
+++ cmd.sh
@@ -2,9 +2,12 @@
 set -e
 
 if [ "$ENV" = 'DEV' ]; then
-    echo "Runnind dev server"
+    echo "Running dev server"
     exec python "identidock.py"
+elif [ "$ENV" = 'TEST' ]; then
+    echo "Running unit tests"
+    exec python "tests.py"
 else
-    echo "Runnind prod server"
+    echo "Running prod server"
     exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py --callable app --stats 0.0.0.0:9191
 fi
